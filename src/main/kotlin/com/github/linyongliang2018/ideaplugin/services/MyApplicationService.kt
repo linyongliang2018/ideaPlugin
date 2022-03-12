90db4d3e0b914fd435e7a8b5aeb8e5e2d3a67313
GitHub Action <action@github.com>
2022-03-12 14:29:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/linyongliang2018/ideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.linyongliang2018.ideaplugin.services
+
+import com.github.linyongliang2018.ideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
